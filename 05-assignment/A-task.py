320a309c012cb6b375cbf1d94ec158526cf9b295
ajoino <jacob.nilsson@ltu.se>
2019-11-11 11:45:23 +0000

    Finalized the assignment

--- 05-assignment/A-task.py
+++ 05-assignment/A-task.py
@@ -44,20 +44,27 @@ def point_1(data):
 def point_2(fit, summary, fit_model):
     num_posterior_samples = (fit.sim['iter'] - fit.sim['warmup']) * fit.sim['chains']
     #random_new_person_index = np.random.randint(num_posterior_samples)
-    new_person_mu = fit.extract('mu')['mu'][np.random.randint(num_posterior_samples)]
-    new_person_tau = fit.extract('tau')['tau'][np.random.randint(num_posterior_samples)]
-    new_person_sigma = fit.extract('sigma')['sigma'][np.random.randint(num_posterior_samples)]
 
-    new_person_theta = np.random.normal(new_person_mu, new_person_tau)
+    num_new_people = 10000
+    random_person_samples = np.zeros((num_new_people, ))
+    for i in range(num_new_people):
+        new_person_mu = fit.extract('mu')['mu'][np.random.randint(num_posterior_samples)]
+        new_person_tau = fit.extract('tau')['tau'][np.random.randint(num_posterior_samples)]
+        new_person_sigma = fit.extract('sigma')['sigma'][np.random.randint(num_posterior_samples)]
 
-    num_new_samples = 20
-    sample_log_reaction_times = np.random.normal(new_person_theta, new_person_sigma, (num_new_samples, ))
-    sample_reaction_times = np.exp(sample_log_reaction_times)
+        new_person_theta = np.random.normal(new_person_mu, new_person_tau)
+        sample_log_reaction_times = np.random.normal(new_person_theta, new_person_sigma, (1, ))
+        #sample_reaction_times = np.exp(sample_log_reaction_times)
+        random_person_samples[i] = np.exp(sample_log_reaction_times)
 
-    plt.subplot(2, 1, 1)
-    plt.scatter(np.arange(num_new_samples)+1, sample_reaction_times)
-    plt.subplot(2, 1, 2)
-    sns.distplot(sample_reaction_times, bins=10, norm_hist=True)
+    random_person_hdi = hdi(random_person_samples, 0.95)
+    print(f'Expected value of randomly drawn people: {np.mean(random_person_samples):.2f}')
+    print(f'HDI of randomly drawn people samples: {random_person_hdi}')
+
+
+    plt.figure()
+    sns.distplot(random_person_samples, bins=20, norm_hist=True)
+    plt.plot(random_person_hdi, [0, 0], 'k', linewidth=5)
     plt.savefig('05-assignment/point_2.png')
 
 def point_3(fit, summary, fit_model, data):
@@ -69,8 +76,26 @@ def point_3(fit, summary, fit_model, data):
 
     print(f'Bayesian group mean:\t\t {group_bayesian_mean:.2f}')
     print(f'Frequentist group mean:\t\t {group_frequentist_mean:.2f}')
-    print(f'Difference of group means:\t {group_bayesian_mean - group_frequentist_mean:.2f}')
-    print(f'Individual bayesian means smaller than individual frequentist means:\n\t\t{np.sum(bayesian_mean[:, np.newaxis] - frequentist_mean < 0)}/{bayesian_mean.shape[0]}')
+    #print(f'Difference of group means:\t {group_bayesian_mean - group_frequentist_mean:.2f}')
+    #print(f'Individual bayesian means smaller than individual frequentist means:\n\t\t{np.sum(bayesian_mean[:, np.newaxis] - frequentist_mean < 0)}/{bayesian_mean.shape[0]}')
+
+    print(frequentist_mean)
+    print(bayesian_mean)
+    plt.figure()
+    plt.plot([group_bayesian_mean, group_bayesian_mean], [0, 34], '--k')
+    for user, (user_bayesian_mean, user_frequentist_mean) in enumerate(zip(bayesian_mean, frequentist_mean)):
+        plt.plot([user_bayesian_mean, user_bayesian_mean], [user, user+1], 'k')
+        plt.plot([user_frequentist_mean, user_frequentist_mean], [user, user+1], ':k')
+        if user != 0:
+            plt.plot([200, 650], [user, user], 'k')
+
+    plt.savefig('05-assignment/point_3.png')
+
+    bayesian_distance = np.abs(group_bayesian_mean - bayesian_mean)[:, np.newaxis]
+    print(bayesian_distance.shape)
+    frequentist_distance = np.abs(group_bayesian_mean - frequentist_mean)
+    print(frequentist_distance.shape)
+    print(f'Individual bayesian means closer to the bayesian mean than individual frequentist means:\n\t\t{np.sum(bayesian_distance < frequentist_distance)}/34')
 
 if __name__ == '__main__':
     with open('05-assignment/data.json') as json_file:
